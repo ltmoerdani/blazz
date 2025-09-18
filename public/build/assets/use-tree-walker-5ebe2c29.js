b6f8d9096ffa4d4c146d9e32954b1c1d06cb8e4a
ltmoerdani <ltmoerdani@gmail.com>
2025-09-18 15:57:48 +0700

    Refactor code structure for improved readability and maintainability

--- public/build/assets/use-tree-walker-5ebe2c29.js
+++
@@ -1,1 +0,0 @@
-import{d as f}from"./keyboard-cedb46ac.js";import{S as d}from"./app-0c4c5d77.js";function N({container:e,accept:r,walk:i,enabled:t}){d(()=>{let a=e.value;if(!a||t!==void 0&&!t.value)return;let c=f(e);if(!c)return;let n=Object.assign(l=>r(l),{acceptNode:r}),o=c.createTreeWalker(a,NodeFilter.SHOW_ELEMENT,n,!1);for(;o.nextNode();)i(o.currentNode)})}export{N as i};
